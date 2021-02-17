3a49a831f4f78518c6a41969362212e83c9230fc
Abhishek Sah <abhi.sah.97@gmail.com>
2021-02-17 19:03:54 +0530

    ci: build project after test job

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+NAME="github.com/odpf/siren"
+VERSION=$(git describe --always --tags 2>/dev/null)
+
+SYS=("linux" "darwin")
+ARCH=("386" "amd64")
+BUILD_DIR="dist"
+
+build() {
+    EXECUTABLE_NAME=$1
+    LD_FLAGS=$2
+    for os in ${SYS[*]}; do
+        for arch in ${ARCH[*]}; do
+
+            # create a folder named via the combination of os and arch
+            TARGET="./$BUILD_DIR/$EXECUTABLE_NAME/${os}-${arch}"
+            mkdir -p $TARGET
+
+            # place the executable within that folder
+            executable="${TARGET}/$EXECUTABLE_NAME"
+            echo $executable
+            GOOS=$os GOARCH=$arch go build -ldflags "$LD_FLAGS" -o $executable $NAME
+        done
+    done
+}
+
+build siren "-X main.Version=${VERSION}"
