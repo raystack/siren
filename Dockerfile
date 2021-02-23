51943e1f258bbc479efa77b938f560f1a5d8f00f
Abhishek Sah <abhishek.sah@go-jek.com>
2021-02-23 16:58:37 +0530

    refactor: cleanup docker build and boilerplate code
    Co-authored-by: Akarsh <akarsh.satija@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM golang:1.15
 WORKDIR /go/src/github.com/odpf/siren
 COPY . .
+RUN make test
 RUN make dist
 
 FROM alpine:latest
@@ -8,6 +9,5 @@ RUN ["apk", "update"]
 RUN ["apk", "add", "libc6-compat"]
 WORKDIR /root/
 COPY --from=0 /go/src/github.com/odpf/siren .
-RUN ls .
 EXPOSE 3000
 ENTRYPOINT ["/root/dist/linux-amd64/siren"]
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,26 +2,21 @@
 NAME="github.com/odpf/siren"
 VERSION=$(git describe --always --tags 2>/dev/null)
 
-SYS=("linux" "darwin")
-ARCH=("386" "amd64")
+SYS="linux"
+ARCH="amd64"
 BUILD_DIR="dist"
 
 build() {
     EXECUTABLE_NAME=$1
     LD_FLAGS=$2
-    for os in ${SYS[*]}; do
-        for arch in ${ARCH[*]}; do
+    # create a folder named via the combination of os and arch
+    TARGET="./$BUILD_DIR/${SYS}-${ARCH}"
+    mkdir -p $TARGET
 
-            # create a folder named via the combination of os and arch
-            TARGET="./$BUILD_DIR/${os}-${arch}"
-            mkdir -p $TARGET
-
-            # place the executable within that folder
-            executable="${TARGET}/$EXECUTABLE_NAME"
-            echo $executable
-            GOOS=$os GOARCH=$arch go build -ldflags "$LD_FLAGS" -o $executable $NAME
-        done
-    done
+    # place the executable within that folder
+    executable="${TARGET}/$EXECUTABLE_NAME"
+    echo $executable
+    GOOS=$SYS GOARCH=$ARCH go build -ldflags "$LD_FLAGS" -o $executable $NAME
 }
 
 build siren "-X main.Version=${VERSION}"
