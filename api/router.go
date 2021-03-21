3354cf92bf9279775c5368c12ce912b4b23f904b
Rohil Surana <rohilsurana96@gmail.com>
2021-03-21 23:17:20 +0530

    add: New Relic integration for gorilla mux

--- api/router.go
+++ api/router.go
@@ -1,16 +1,23 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/gorilla/mux"
+	"github.com/newrelic/go-agent/v3/integrations/nrgorilla"
+	"github.com/newrelic/go-agent/v3/newrelic"
 	"github.com/odpf/siren/api/handlers"
 	"github.com/odpf/siren/service"
 )
 
 // New initializes the service router
-func New(container *service.Container) *mux.Router {
+func New(container *service.Container, nr *newrelic.Application) *mux.Router {
 	r := mux.NewRouter().StrictSlash(true)
 
+	nrMiddleware := nrgorilla.Middleware(nr)
+
+	r.Use(nrMiddleware)
 	r.Use(logger)
 
 	// Route => handler
@@ -31,6 +38,12 @@ func New(container *service.Container) *mux.Router {
 
 	r.Methods("PUT").Path("/rules").Handler(handlers.UpsertRule(container.RulesService))
 	r.Methods("GET").Path("/rules").Handler(handlers.GetRules(container.RulesService))
+
+	// Handle middlewares for NotFoundHandler and MethodNotAllowedHandler since Mux doesn't apply middlewares to them. Ref: https://github.com/gorilla/mux/issues/416
+	_, r.NotFoundHandler = newrelic.WrapHandle(nr, "NotFoundHandler", logger(http.NotFoundHandler()))
+	_, r.MethodNotAllowedHandler = newrelic.WrapHandle(nr, "MethodNotAllowedHandler", logger(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusMethodNotAllowed)
+	})))
 
 	return r
 }
