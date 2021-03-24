7f0e196d043d05b49519b4cc60a45bae72e1ae7b
kevin.bheda <kevin.bheda@go-jek.com>
2021-03-24 11:01:23 +0530

    docs: get api for credentials

--- docs/alertCredentials.go
+++ docs/alertCredentials.go
@@ -2,21 +2,37 @@ package docs
 
 import "github.com/odpf/siren/domain"
 
-// swagger:response templatesResponse
-type alertCredentialResponse struct {
-	// in:body
-	Body domain.AlertCredential
+type AlertCredentialResponse struct {
+	Entity               string             `json:"entity"`
+	PagerdutyCredentials string             `json:"pagerduty_credentials"`
+	SlackConfig          domain.SlackConfig `json:"slack_config"`
 }
 
 //-------------------------
-// swagger:route PUT //alertingCredentials/teams/{teamName}:  alertcredential createAlertCredentialRequest
+// swagger:route PUT /alertingCredentials/teams/{teamName}  alertcredential createAlertCredentialRequest
 // Upsert AlertCredentials API: This API helps in creating or updating the teams slack and pagerduty credentials
 //responses:
-//   200: emptyResponse
+//   200:
 
-// swagger:parameters createTemplateRequest
+// swagger:parameters createAlertCredentialRequest
 type createAlertCredentialRequest struct {
 	// Create AlertCredential request
 	// in:body
-	Body domain.AlertCredential
+	Body AlertCredentialResponse
+	// name of the team
+	// in:path
+	TeamName string `json:"teamName"`
+}
+
+//-------------------------
+// swagger:route GET /alertingCredentials/teams/{teamName}  alertcredential getAlertCredentialRequest
+// Get AlertCredentials API: This API helps in getting the teams slack and pagerduty credentials
+//responses:
+//   200: AlertCredentialResponse
+
+// swagger:parameters getAlertCredentialRequest
+type getAlertCredentialRequest struct {
+	// name of the team
+	// in:path
+	TeamName string `json:"teamName"`
 }
