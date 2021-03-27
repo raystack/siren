4219b4c1f4e67d26104813c455ed9438b297f417
Abhishek Sah <abhi.sah.97@gmail.com>
2021-03-27 16:50:17 +0530

    refactor: decorate output of uploader command and add tests

--- cmd/upload.go
+++ cmd/upload.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"github.com/odpf/siren/app"
+	"github.com/odpf/siren/client"
 	"github.com/odpf/siren/uploader"
 	"github.com/spf13/cobra"
 )
@@ -17,5 +20,44 @@ func init() {
 func uploadRules(cmd *cobra.Command, args []string) error {
 	c := app.LoadConfig()
 	s := uploader.NewService(&c.SirenService)
-	return s.Upload(args[0])
+	result, err := s.Upload(args[0])
+	//print all resources(succeed or failed in upsert)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	switch obj := result.(type) {
+	case *client.Template:
+		printTemplate(obj)
+	case []*client.Rule:
+		printRules(obj)
+	default:
+		return errors.New("unknown response")
+	}
+	return nil
+}
+
+func printRules(rules []*client.Rule) {
+	for i := 0; i < len(rules); i++ {
+		fmt.Println("Upserted Rule")
+		fmt.Println("ID:", rules[i].Id)
+		fmt.Println("Name:", rules[i].Name)
+		fmt.Println("Name:", rules[i].Status)
+		fmt.Println("Created At:", rules[i].CreatedAt)
+		fmt.Println("Updated At:", rules[i].UpdatedAt)
+		fmt.Println()
+	}
+}
+
+func printTemplate(template *client.Template) {
+	if template == nil {
+		return
+	}
+	fmt.Println("Upserted Template")
+	fmt.Println("ID:", template.Id)
+	fmt.Println("Name:", template.Name)
+	fmt.Println("Created At:", template.CreatedAt)
+	fmt.Println("Updated At:", template.UpdatedAt)
+	fmt.Println("Tags:", template.Tags)
+	fmt.Println("Variables:", template.Variables)
 }
