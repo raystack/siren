fe9b68181e77277ee82ee408faffc99bca1d7edc
Abhishek Sah <abhishek.sah@go-jek.com>
2021-08-30 15:16:23 +0530

    feat: add grpc support (#49)
    * feat: add grpc gateway support
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * feat: create grpc handlers for alert history APIs
    
    Co-authored-by: Praveen Yadav <pyadav9678@gmail.com>
    
    * create grpc handlers for workspace list API
    
    * create grpc handlers for exchange code API
    
    * create grpc handlers for alert credentials APIs
    
    * feat: create grpc handlers for notification API
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * feat: create grpc handlers for rules APIs
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * feat: create grpc handlers for template APIs and remove associated routes
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * test: create alert history api
    
    * refactor: request & response payload from camelCase to snake_case
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * feat: add slack blocks in send slack notification grpc api
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * docs: serve swagger docs via http api
    
    * refactor: remove redundant logging config & add missing log statements
    
    Co-authored-by: Praveen Yadav <pyadav9678@gmail.com>
    
    * feat: add newrelic integration for grpc handlers
    
    Co-authored-by: Praveen Yadav <pyadav9678@gmail.com>
    
    * fix: return success response when create alert history fails due to invalid parameters
    
    Co-authored-by: Praveen Yadav <pyadav9678@gmail.com>
    
    * refactor: fix linting and formatting issues
    
    Co-authored-by: Praveen Yadav <pyadav9678@gmail.com>
    
    * refactor: follow grpc naming conventions
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * fix: change buf generate path to siren
    
    Co-authored-by: Abhishek <abhi.sah.97@gmail.com>
    
    * fix: add siren executable in gitignore
    
    * refactor: change Alerts to singular word Alert
    
    Co-authored-by: Praveen Yadav <pyadav9678@gmail.com>

--- app/server.go
+++ app/server.go
@@ -1,19 +1,39 @@
 package app
 
 import (
+	"context"
+	"embed"
 	"fmt"
+	"github.com/go-openapi/runtime/middleware"
+	"google.golang.org/protobuf/encoding/protojson"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	cortexClient "github.com/grafana/cortex-tools/pkg/client"
-
-	"github.com/odpf/siren/api"
-	"github.com/odpf/siren/domain"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	v1 "github.com/odpf/siren/api/handlers/v1"
+	pb "github.com/odpf/siren/api/proto/odpf/siren"
 	"github.com/odpf/siren/logger"
 	"github.com/odpf/siren/metric"
+	"golang.org/x/net/http2"
+	"google.golang.org/grpc"
+
+	"github.com/newrelic/go-agent/v3/integrations/nrgrpc"
+
+	"github.com/odpf/siren/domain"
 	"github.com/odpf/siren/service"
 	"github.com/odpf/siren/store"
-	"go.uber.org/zap"
+	"golang.org/x/net/http2/h2c"
 )
+
+//go:embed siren.swagger.json
+var swaggerFile embed.FS
 
 // RunServer runs the application server
 func RunServer(c *domain.Config) error {
@@ -44,10 +64,81 @@ func RunServer(c *domain.Config) error {
 	if err != nil {
 		return err
 	}
-	r := api.New(services, nr, logger)
 
-	logger.Info("starting server", zap.Int("port", c.Port))
-	return http.ListenAndServe(fmt.Sprintf(":%d", c.Port), r)
+	loggerOpts := []grpc_zap.Option{grpc_zap.WithLevels(grpc_zap.DefaultCodeToLevel)}
+
+	// init grpc server
+	opts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+			grpc_recovery.UnaryServerInterceptor(),
+			grpc_ctxtags.UnaryServerInterceptor(),
+			nrgrpc.UnaryServerInterceptor(nr),
+			grpc_zap.UnaryServerInterceptor(logger, loggerOpts...),
+		)),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpc_recovery.StreamServerInterceptor(),
+			grpc_ctxtags.StreamServerInterceptor(),
+			nrgrpc.StreamServerInterceptor(nr),
+			grpc_zap.StreamServerInterceptor(logger),
+		)),
+	}
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterSirenServiceServer(grpcServer, v1.NewGRPCServer(services, nr, logger))
+
+	// init http proxy
+	timeoutGrpcDialCtx, grpcDialCancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer grpcDialCancel()
+
+	gwmux := runtime.NewServeMux(
+		runtime.WithErrorHandler(runtime.DefaultHTTPErrorHandler),
+		runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.HTTPBodyMarshaler{
+			Marshaler: &runtime.JSONPb{
+				MarshalOptions: protojson.MarshalOptions{
+					UseProtoNames: true,
+				},
+			},
+		}),
+	)
+	address := fmt.Sprintf(":%d", c.Port)
+	grpcConn, err := grpc.DialContext(timeoutGrpcDialCtx, address, grpc.WithInsecure())
+	if err != nil {
+		return err
+	}
+
+	runtimeCtx, runtimeCancel := context.WithCancel(context.Background())
+	defer runtimeCancel()
+
+	if err := pb.RegisterSirenServiceHandler(runtimeCtx, gwmux, grpcConn); err != nil {
+		return err
+	}
+
+	baseMux := http.NewServeMux()
+	baseMux.HandleFunc("/siren.swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		http.FileServer(http.FS(swaggerFile)).ServeHTTP(w, r)
+		return
+	})
+	baseMux.Handle("/documentation", middleware.SwaggerUI(middleware.SwaggerUIOpts{
+		SpecURL: "/siren.swagger.json",
+		Path:    "documentation",
+	}, http.NotFoundHandler()))
+	baseMux.Handle("/", gwmux)
+
+	server := &http.Server{
+		Handler:      grpcHandlerFunc(grpcServer, baseMux),
+		Addr:         address,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	log.Println("server running on port:", c.Port)
+	if err := server.ListenAndServe(); err != nil {
+		if err != http.ErrServerClosed {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func RunMigrations(c *domain.Config) error {
@@ -72,4 +163,14 @@ func RunMigrations(c *domain.Config) error {
 
 	services.MigrateAll(store)
 	return nil
+}
+
+func grpcHandlerFunc(grpcServer *grpc.Server, otherHandler http.Handler) http.Handler {
+	return h2c.NewHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.Contains(r.Header.Get("Content-Type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			otherHandler.ServeHTTP(w, r)
+		}
+	}), &http2.Server{})
 }
--- go.mod
+++ go.mod
@@ -9,9 +9,13 @@ require (
 	github.com/go-openapi/runtime v0.19.26
 	github.com/go-openapi/spec v0.20.2 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/handlers v1.5.1 // indirect
-	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/grafana/cortex-tools v0.7.2
+	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
+	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.5.0
 	github.com/gtank/cryptopasta v0.0.0-20170601214702-1f550f6f2f69
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jeremywohl/flatten v1.0.1
@@ -20,13 +24,14 @@ require (
 	github.com/magiconair/properties v1.8.4 // indirect
 	github.com/mcuadros/go-defaults v1.2.0
 	github.com/mitchellh/mapstructure v1.4.1
-	github.com/newrelic/go-agent/v3 v3.11.0
+	github.com/newrelic/go-agent/v3 v3.12.0
 	github.com/newrelic/go-agent/v3/integrations/nrgorilla v1.1.0
+	github.com/newrelic/go-agent/v3/integrations/nrgrpc v1.3.1
 	github.com/pelletier/go-toml v1.8.1 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/alertmanager v0.21.1-0.20200911160112-1fdff6b3f939
 	github.com/prometheus/prometheus v1.8.2-0.20201014093524-73e2ce1bd643
-	github.com/purini-to/zapmw v1.1.0
+	github.com/purini-to/zapmw v1.1.0 // indirect
 	github.com/slack-go/slack v0.9.3
 	github.com/spf13/afero v1.4.1 // indirect
 	github.com/spf13/cast v1.3.1 // indirect
@@ -36,8 +41,11 @@ require (
 	github.com/stretchr/testify v1.6.1
 	go.uber.org/zap v1.14.1
 	golang.org/x/crypto v0.0.0-20201124201722-c8d3bf9c5392 // indirect
-	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
-	golang.org/x/sys v0.0.0-20201126233918-771906719818 // indirect
+	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d
+	golang.org/x/oauth2 v0.0.0-20210615190721-d04028783cf1
+	google.golang.org/genproto v0.0.0-20210617175327-b9e0b3197ced
+	google.golang.org/grpc v1.38.0
+	google.golang.org/protobuf v1.26.0
 	gopkg.in/go-playground/validator.v9 v9.31.0
 	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
