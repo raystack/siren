7a83d4ae494d62e40f60717993129673a6fa5338
Ravi Suhag <suhag.ravi@gmail.com>
2022-01-18 01:04:54 -0800

    feat: accept config as flag for serve and migrate commands (#85)
    * feat: move to salt config
    
    * chore: move sample config to config folder
    
    * chore: add sample config

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -7,12 +7,23 @@ import (
 )
 
 func migrateCmd() *cobra.Command {
-	return &cobra.Command{
+	var configFile string
+
+	cmd := &cobra.Command{
 		Use:   "migrate",
 		Short: "Migrate database schema",
+		Annotations: map[string]string{
+			"group:other": "dev",
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			c := config.LoadConfig()
-			return app.RunMigrations(c)
+			cfg, err := config.LoadConfig(configFile)
+			if err != nil {
+				return err
+			}
+			return app.RunMigrations(cfg)
 		},
 	}
+
+	cmd.Flags().StringVarP(&configFile, "config", "c", "./config.yaml", "Config file path")
+	return cmd
 }
