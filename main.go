acadfd18c9ef4a738c8a2e2542d8ac8c73cfec9e
Abduh <mabdh@users.noreply.github.com>
2023-05-11 11:22:05 +0700

    feat(cli): return os error status code 1 when siren cli return error (#7)

--- main.go
+++ main.go
@@ -9,11 +9,23 @@ import (
 	"github.com/goto/siren/cli"
 )
 
+const (
+	exitError = 1
+	exitOK    = 0
+)
+
 func main() {
+	os.Exit(execute())
+}
+
+func execute() int {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
 	if err := cli.New().ExecuteContext(ctx); err != nil {
-		fmt.Printf("%+v\n", err)
+		fmt.Fprintf(os.Stderr, "\n%v\n", err)
+		return exitError
 	}
+
+	return exitOK
 }
